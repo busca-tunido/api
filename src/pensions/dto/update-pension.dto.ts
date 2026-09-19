fe3de32941d8d9abaa72fb670d3b394ae2ef63c5
Jose Leiva <email.joseleiva@gmail.com>
2026-09-19 17:33:00 -0300

    feat(pensions): add optional isActive field to UpdatePensionDto

--- src/pensions/dto/update-pension.dto.ts
+++ src/pensions/dto/update-pension.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreatePensionDto } from './create-pension.dto.js';
 
-export class UpdatePensionDto extends PartialType(CreatePensionDto) {}
+export class UpdatePensionDto extends PartialType(CreatePensionDto) {
+  @ApiPropertyOptional({ example: true })
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
